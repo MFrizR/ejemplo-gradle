c9ec86b9096686927e9cc7db9b9ad27ff3967af7
Mfrizr <mafriz@ing.ucsc.cl>
2022-01-29 23:38:34 -0300

    unificar

--- maven.groovy
+++ maven.groovy
@@ -14,12 +14,14 @@ def call(){
   stage("Paso 3: Build .Jar"){
     sh "mvn clean package -e"
   }
-  stage("Paso 4: Sonar - Análisis Estático"){
-      sh "echo 'Análisis Estático!'"
-      withSonarQubeEnv('sonarqube3') {
-          sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
-      }
-  }
+stage("Paso 2: Sonar - Análisis Estático"){
+                        sh "echo 'Análisis Estático!'"
+                        withSonarQubeEnv('sonarqube3') {
+                            sh "echo 'Calling sonar by ID!'"
+                            // Run Maven on a Unix agent to execute Sonar.
+                            sh './gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+                        }
+                    }
   stage("Paso 5: Curl Springboot Gradle sleep 20"){
       sh "gradle bootRun&"
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
